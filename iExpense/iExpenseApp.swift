eaeaafa64530d652880ae86c25b7306ce7565fa4
AskNilesh <rathodnilsrk@gmail.com>
2023-05-04 21:05:04 +0530

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Nilesh Rathod on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
